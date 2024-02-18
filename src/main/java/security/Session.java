6c1d4b509fc21536fd5a5ad51a590203354f718b
SyrineTrablesi <syrinet6@gmail.com>
2024-02-18 23:01:40 +0100

    Singleton pour la classe Session

--- src/main/java/security/Session.java
+++ src/main/java/security/Session.java
@@ -4,17 +4,34 @@ import entities.User;
 import services.ServiceUser;
 
 public class Session {
-     public  static UserInfo userInfo;
-     ServiceUser  serviceuser =new ServiceUser();
-     //Methode login
-    public void login(String email,String password){
+    private static Session instance;
+    private UserInfo userInfo;
+    private ServiceUser serviceuser = new ServiceUser();
+
+    // Constructeur privé pour empêcher l'instanciation directe depuis l'extérieur de la classe
+    private Session() {}
+
+    // Méthode statique pour obtenir l'instance unique de Session
+    public static Session getInstance() {
+        if (instance == null) {
+            instance = new Session();
+        }
+        return instance;
+    }
+
+    // Méthode login
+    public void login(String email, String password) {
         try {
-            User user =serviceuser.connexion(email,password);
-            userInfo=new UserInfo(user.getId(), user.getNom(), user.getPrenom(), user.getRole(), user.getEmail(), user.getPassword());
+            User user = serviceuser.connexion(email, password);
+            userInfo = new UserInfo(user.getId(), user.getNom(), user.getPrenom(), user.getRole(), user.getEmail(), user.getPassword());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
+    // Autres méthodes pour accéder aux informations de session
+    public UserInfo getUserInfo() {
+        return userInfo;
+    }
+}
 
- }
